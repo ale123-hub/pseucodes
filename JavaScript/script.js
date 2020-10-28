3a2c9b74af4d093e97925e99e82bcb62fd540258
Liseth251 <70977143+Liseth251@users.noreply.github.com>
2020-10-28 08:24:30 -0500

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,49 @@
+window.onload= $(document).ready(function(){
+    $('.secciones article').hide();
+    $('.secciones article:first').show();
+
+    $('ul.tabs li a').click(function(){
+        $('ul.tabs li a').removeClass();
+        $(this).addClass('active');
+        $('.secciones article').hide()
+
+        var activeTab = $(this).attr('href');
+        $(activeTab).show();
+        return false;
+    });
+});
+
+//tab1
+window.onload= $(document).ready(function(){ 
+    $('ul.tabs1 li a :first').addClass('active');
+    $('.secciones1 article').hide();
+    $('.secciones1 article:first').show();
+
+    $('ul.tabs1 li a').click(function(){
+        $('ul.tabs1 li a').removeClass();
+        $(this).addClass('active');
+        $('.secciones1 article').hide()
+
+        var activeTab1 = $(this).attr('href');
+        $(activeTab1).show();
+        return false;
+    });
+});
+
+//tab2
+window.onload= $(document).ready(function(){
+    $('ul.tabs2 li a:first').addClass('active');
+    $('.secciones2 article').hide();
+    $('.secciones2 article:first').show();
+
+    $('ul.tabs2 li a').click(function(){
+        $('ul.tabs2 li a').removeClass();
+        $(this).addClass('active');
+        $('.secciones2 article').hide()
+
+        var activeTab2 = $(this).attr('href');
+        $(activeTab2).show();
+        return false;
+    });
+});
+
